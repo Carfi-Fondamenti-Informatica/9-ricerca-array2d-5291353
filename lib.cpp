9785239bca27741af69ab45d59642374532d7a7c
Francesco <91691097+5291353@users.noreply.github.com>
2021-12-11 14:08:26 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "string.h"
+#include "lib.h"
+using namespace std;
+bool posizione(char A[10][20], char B[10],int &b){
+    for(int i=0;i<10;i++){
+        if(strcmp(A[i],B)==0){
+            b=i;
+            return true;
+        }
+    }
+    return false;
+}
