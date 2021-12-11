375bb4849794442f3f02ea0cde9aa0d31d147612
Francesco <91691097+5291353@users.noreply.github.com>
2021-12-11 14:10:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int b=0;
+    char A[10][20],B[20];
+    for(int i=0;i<10;i++){
+        cin>>A[i];
+
+    }
+    cin>>B;
+    if(posizione(A,B,b)){
+        cout<< b <<endl;
+    }else{
+        cout<<"non presente"<<endl;
+    }
+
+    return 0;
 }
